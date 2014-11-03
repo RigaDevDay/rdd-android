6db6b76c9bec5e559e560c36ea538e585b78bb61
Alexey Buzdin <alex.buzdin@gmail.com>
2014-11-03 02:25:27 +0200

    Added Speakers GridView

--- app/src/main/java/lv/rigadevday/android/ui/speakers/SpeakersFragment.java
+++ app/src/main/java/lv/rigadevday/android/ui/speakers/SpeakersFragment.java
@@ -1,11 +1,21 @@
 package lv.rigadevday.android.ui.speakers;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
+import javax.inject.Inject;
+
+import butterknife.InjectView;
 import lv.rigadevday.android.R;
 import lv.rigadevday.android.ui.BaseFragment;
 
 public class SpeakersFragment extends BaseFragment {
+
+    @Inject
+    SpeakersGridAdapter gridAdapter;
+
+    @InjectView(R.id.schedule_grid)
+    GridView gridView;
 
     @Override
     protected int contentViewId() {
@@ -15,5 +25,12 @@ public class SpeakersFragment extends BaseFragment {
     @Override
     protected void init(Bundle savedInstanceState) {
         super.init(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        gridView.setAdapter(gridAdapter);
     }
 }
---
+++ app/src/main/java/lv/rigadevday/android/ui/speakers/SpeakersGridAdapter.java
@@ -0,0 +1,57 @@
+package lv.rigadevday.android.ui.speakers;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import javax.inject.Inject;
+
+import lv.rigadevday.android.R;
+import lv.rigadevday.android.common.TypefaceCache;
+import lv.rigadevday.android.domain.Speaker;
+import lv.rigadevday.android.ui.ViewHolder;
+
+public class SpeakersGridAdapter extends ArrayAdapter<Speaker> {
+
+    LayoutInflater inflater;
+    TypefaceCache typefaceCache;
+
+    @Inject
+    public SpeakersGridAdapter(Context context, LayoutInflater inflater, TypefaceCache typefaceCache) {
+        super(context, 0, Speaker.getAll());
+        this.inflater = inflater;
+        this.typefaceCache = typefaceCache;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = convertView != null ? convertView : inflater.inflate(R.layout.schedule_item, parent, false);
+
+        Speaker item = getItem(position);
+        pupulateView(convertView, item);
+
+        return convertView;
+    }
+
+    private void pupulateView(View convertView, Speaker item) {
+        TextView title = ViewHolder.get(convertView, R.id.item_title);
+        title.setTypeface(typefaceCache.loadFromAsset("fonts/RobotoCondensed-Regular.ttf"));
+        title.setText(item.getName());
+
+        TextView time = ViewHolder.get(convertView, R.id.item_time_place);
+        time.setTypeface(typefaceCache.loadFromAsset("fonts/Roboto-Light.ttf"));
+        time.setText(item.getCompany());
+
+//        ImageView bookmark = ViewHolder.get(convertView, R.id.item_bookmark);
+//        bookmark.setVisibility(item.isBookmarked() ? View.VISIBLE : View.INVISIBLE);
+
+        // TODO: implement placeholder arawable assigning to presentations
+        ImageView image = ViewHolder.get(convertView, R.id.item_image);
+        int drawableId =  R.drawable.pic_2_edited;
+        image.setImageDrawable(getContext().getResources().getDrawable(drawableId));
+    }
+}
