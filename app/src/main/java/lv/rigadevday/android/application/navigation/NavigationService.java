8a846ce9fc29ab71c627a6224cb98ec63ea0c37b
echoAlexey <alexey@tcherezov.com>
2014-12-29 22:02:18 +0200

    Change menu items order

--- app/src/main/java/lv/rigadevday/android/application/navigation/NavigationService.java
+++ app/src/main/java/lv/rigadevday/android/application/navigation/NavigationService.java
@@ -22,13 +22,13 @@ public class NavigationService {
     }
 
     public List<NavigationOption> getDrawerNavigationOptions() {
-        return new ArrayList<NavigationOption>(Arrays.asList(
+        return new ArrayList<>(Arrays.asList(
                 new NavigationOption(R.string.schedule, R.drawable.icon_menu_schedule, ScheduleFragment.class),
-                new NavigationOption(R.string.talks, R.drawable.icon_menu_talks, TalkFragment.class),
                 new NavigationOption(R.string.speakers, R.drawable.icon_menu_speakers, SpeakersFragment.class),
-                new NavigationOption(R.string.venue, R.drawable.icon_menu_venue, VenueFragment.class),
+                new NavigationOption(R.string.talks, R.drawable.icon_menu_talks, TalkFragment.class),
+                new NavigationOption(R.string.bookmarks, R.drawable.icon_menu_bookmark, BookmarkFragment.class),
                 new NavigationOption(R.string.organizers, R.drawable.icon_menu_organizers, OrganizerFragment.class),
-                new NavigationOption(R.string.bookmarks, R.drawable.icon_menu_bookmark, BookmarkFragment.class)
+                new NavigationOption(R.string.venue, R.drawable.icon_menu_venue, VenueFragment.class)
         ));
     }
 }
