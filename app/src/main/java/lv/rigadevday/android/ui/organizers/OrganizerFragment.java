9f6c10a6fafdb615807d5d33ac1f53b46f7cbc7b
echoAlexey <alexey@tcherezov.com>
2014-12-29 21:41:28 +0200

    Add organizers screen: - Add organizers - Show dialog with organizer information - Show main conference sponsors

--- app/src/main/java/lv/rigadevday/android/ui/organizers/OrganizerFragment.java
+++ app/src/main/java/lv/rigadevday/android/ui/organizers/OrganizerFragment.java
@@ -2,6 +2,9 @@ package lv.rigadevday.android.ui.organizers;
 
 import android.os.Bundle;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+
+import butterknife.OnClick;
 import lv.rigadevday.android.R;
 import lv.rigadevday.android.ui.BaseFragment;
 
@@ -15,5 +18,31 @@ public class OrganizerFragment extends BaseFragment {
     @Override
     protected void init(Bundle savedInstanceState) {
         super.init(savedInstanceState);
+    }
+
+    @OnClick(R.id.jug)
+    public void showJugDialog() {
+        showDetailsDialog(R.string.jug_name, R.string.jug_description);
+    }
+
+    @OnClick(R.id.gdg)
+    public void showGdgDialog() {
+        showDetailsDialog(R.string.gdg_name, R.string.gdg_description);
+    }
+
+    @OnClick(R.id.lvoug)
+    public void showLvougDialog() {
+        showDetailsDialog(R.string.lvoug_name, R.string.lvoug_description);
+    }
+
+    private void showDetailsDialog(int titleRes, int descriptionRes) {
+        new MaterialDialog.Builder(getActivity())
+                .title(titleRes)
+                .titleColorRes(R.color.primary)
+                .content(descriptionRes)
+                .contentColorRes(R.color.color_404040)
+                .positiveColorRes(R.color.cerulean)
+                .positiveText(R.string.OK)
+                .show();
     }
 }
