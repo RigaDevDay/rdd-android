623d66e2bd387fd4c6d2518ab3cfa3ecf489b0b7
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2015-12-21 15:30:10 +0200

    Put basic data to assets to start work on ui

--- app/src/main/java/lv/rigadevday/android/v2/ui/schedule/ScheduleFragment.java
+++ app/src/main/java/lv/rigadevday/android/v2/ui/schedule/ScheduleFragment.java
@@ -2,12 +2,19 @@ package lv.rigadevday.android.v2.ui.schedule;
 
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
+import butterknife.Bind;
 import butterknife.OnClick;
 import de.greenrobot.event.EventBus;
 import lv.rigadevday.android.R;
+import lv.rigadevday.android.v2.model.Days;
 import lv.rigadevday.android.v2.ui.base.BaseFragment;
 import lv.rigadevday.android.v2.navigation.OpenTalkEvent;
 
@@ -17,6 +24,9 @@ public class ScheduleFragment extends BaseFragment implements ScheduleFragmentPr
 
     @Inject
     EventBus mBus;
+
+    @Bind(R.id.schedule_label)
+    TextView mLabel;
 
     private ScheduleFragmentController mController;
 
@@ -41,5 +51,27 @@ public class ScheduleFragment extends BaseFragment implements ScheduleFragmentPr
         mController.buttonClicked();
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mController.onResume();
+        mController.fetch();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mController.onPause();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void setupList(List<Days> data) {
+        mLabel.setText(data.toString());
+    }
 }
 
--- app/src/main/java/lv/rigadevday/android/v2/ui/schedule/ScheduleFragmentController.java
+++ app/src/main/java/lv/rigadevday/android/v2/ui/schedule/ScheduleFragmentController.java
@@ -1,10 +1,24 @@
 package lv.rigadevday.android.v2.ui.schedule;
 
+import android.util.Log;
+
+import javax.inject.Inject;
+
+import lv.rigadevday.android.v2.model.DataRoot;
+import lv.rigadevday.android.v2.networking.DataFetchService;
+import lv.rigadevday.android.v2.networking.DataFetchStub;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 /**
  */
 public class ScheduleFragmentController {
 
+
     private final ScheduleFragmentPresenter mPresenter;
+    private Subscription mDataFetch;
 
     public ScheduleFragmentController(ScheduleFragmentPresenter presenter) {
         mPresenter = presenter;
@@ -12,5 +26,24 @@ public class ScheduleFragmentController {
 
     public void buttonClicked() {
         mPresenter.openTalk();
+    }
+
+    public void fetch() {
+        Log.wtf("DEBUG", "fetch requested!");
+        // TODO make call for DataFetchService.getData() when it is in place
+        mDataFetch = DataFetchStub.getData(mPresenter.getContext())
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .map(data -> data.days)
+                .subscribe(mPresenter::setupList);
+    }
+
+    public void onResume() {
+
+    }
+
+    public void onPause() {
+        if (mDataFetch != null && !mDataFetch.isUnsubscribed())
+            mDataFetch.unsubscribe();
     }
 }
