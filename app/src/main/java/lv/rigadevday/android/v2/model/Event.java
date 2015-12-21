5c3bf262991637e3278e2f929ae551b6fdf4cc3e
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2015-12-21 23:38:37 +0200

    Schedule prototype
    Problem with bit cards and workshops.
    Problem with speaker name retrieval.
    Not clickable yet.

--- app/src/main/java/lv/rigadevday/android/v2/model/Event.java
+++ app/src/main/java/lv/rigadevday/android/v2/model/Event.java
@@ -1,5 +1,8 @@
 package lv.rigadevday.android.v2.model;
 
+import com.annimon.stream.Collectors;
+import com.annimon.stream.Stream;
+
 import java.util.List;
 
 /**
@@ -9,7 +12,12 @@ public class Event {
     public String title;
     public String subtitle;
     public String description;
-    public List<String> speaker;
+    public List<String> speakers;
     public List<String> tags;
 
+    public String speaker() {
+        if (speakers != null)
+            return Stream.of(speakers).collect(Collectors.joining(", "));
+        return "";
+    }
 }
--- app/src/main/java/lv/rigadevday/android/v2/ui/schedule/day/DayScheduleFragment.java
+++ app/src/main/java/lv/rigadevday/android/v2/ui/schedule/day/DayScheduleFragment.java
@@ -1,7 +1,12 @@
 package lv.rigadevday.android.v2.ui.schedule.day;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.TextView;
+
+import java.util.List;
 
 import butterknife.Bind;
 import lv.rigadevday.android.R;
@@ -12,10 +17,20 @@ import lv.rigadevday.android.v2.ui.base.BaseFragment;
  */
 public class DayScheduleFragment extends BaseFragment {
 
-    @Bind(R.id.day_schedule_label)
-    TextView mLabel;
+    @Bind(R.id.day_schedule_recycler)
+    protected RecyclerView mRecycler;
 
-    private Schedule schedule;
+    // Not sure if this is the best way to do things
+    @Bind({
+            R.id.schedule_room_1,
+            R.id.schedule_room_2,
+            R.id.schedule_room_3,
+            R.id.schedule_room_4,
+            R.id.schedule_room_5
+    })
+    protected List<TextView> mRooms;
+
+    private Schedule mSchedule;
 
     public static DayScheduleFragment newInstance(Schedule schedule) {
         DayScheduleFragment fragment = new DayScheduleFragment();
@@ -29,12 +44,25 @@ public class DayScheduleFragment extends BaseFragment {
     }
 
     public void setSchedule(Schedule schedule) {
-        this.schedule = schedule;
+        this.mSchedule = schedule;
     }
 
     @Override
     protected void init(Bundle savedInstanceState) {
         super.init(savedInstanceState);
-        mLabel.setText(schedule.toString());
+
+        setRooms(mSchedule.roomNames);
+
+        mRecycler.setHasFixedSize(true);
+        mRecycler.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        mRecycler.setAdapter(new DayScheduleAdapter(mSchedule.schedule));
     }
+
+    public void setRooms(List<String> rooms) {
+        for (int i = 0; i < rooms.size(); i++) {
+            mRooms.get(i).setVisibility(View.VISIBLE);
+            mRooms.get(i).setText(rooms.get(i));
+        }
+    }
+
 }
