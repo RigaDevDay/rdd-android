58dd5d9649a45933cf3f592bbffd215295301a70
Sergejs Luhmirins <sergejs@fullcontact.com>
2017-01-14 00:21:42 +0200

    Some view utilities

--- app/src/main/kotlin/lv/rigadevday/android/utils/Utils.kt
+++ app/src/main/kotlin/lv/rigadevday/android/utils/Utils.kt
@@ -1,3 +1,17 @@
 package lv.rigadevday.android.utils
 
+import android.support.annotation.LayoutRes
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
 fun String.toExtraKey() = "lv.rigadevday.android.extra.$this"
+
+fun ViewGroup.inflate(@LayoutRes id: Int): View
+    = LayoutInflater.from(this.context).inflate(id, this, false)
+
+fun ImageView.loadImage(url: String) {
+    Glide.with(this.context).load(url).into(this)
+}
