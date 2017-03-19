4ddb15729a0516251bbbfb66cdb2ae8bb7990ee0
Sergejs Luhmirins <sergejs@fullcontact.com>
2017-03-19 15:15:26 +0200

    Utility to quickly find mismatches in sessions

--- app/src/main/kotlin/lv/rigadevday/android/utils/RxUtils.kt
+++ app/src/main/kotlin/lv/rigadevday/android/utils/RxUtils.kt
@@ -4,6 +4,7 @@ import io.reactivex.Flowable
 import io.reactivex.Maybe
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.functions.BiFunction
+import lv.rigadevday.android.repository.Repository
 
 
 fun <T> Iterable<T>.asFlowable(): Flowable<T> = Flowable.fromIterable<T>(this)
@@ -21,3 +22,21 @@ fun <T> Flowable<T>.bindSchedulers(): Flowable<T> = this
     .observeOn(AndroidSchedulers.mainThread())
 
 fun <T, R> biFunction(function: (T, R) -> T): BiFunction<T, R, T> = BiFunction(function)
+
+
+fun findSessionsMismatches(repo: Repository) = repo.schedule()
+    .flatMap { Flowable.fromIterable(it.timeslots) }
+    .flatMap { Flowable.fromIterable(it.sessionIds) }
+    .toList()
+    .map { it.distinct() }
+    .zipWith(repo.sessions().map { it.id }.toList(), biFunction { idsInSchedule, idsInSessions ->
+        "ids in sessions and not in schedule: ".logE()
+        idsInSessions.subtract(idsInSchedule).logD()
+
+        "ids in schedule and not in sessions: ".logE()
+        idsInSchedule.subtract(idsInSessions).logD()
+
+        listOf<Int>()
+    })
+    .toCompletable()
+
